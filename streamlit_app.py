e9aaf5fc5c6c572c075fee9b10af37187a99ed85
AmSchulte <anmschulte@gmail.com>
2022-04-25 20:26:24 +0200

    more data

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,57 +27,59 @@ elif time == '14 days':
 else:
     path = d7
 
-image_folder_path = os.path.join(path, condition, "rat "+str(rat), drg_position, image_type)
-filenames = os.listdir(image_folder_path)
+if rat == 1:
+    image_folder_path = os.path.join(path, condition, "rat "+str(rat), drg_position, image_type)
+    filenames = os.listdir(image_folder_path)
 
-image_number = st.sidebar.slider('Image number', min_value=1, value=1, max_value=len(filenames))
+    image_number = st.sidebar.slider('Image number', min_value=1, value=1, max_value=len(filenames))
 
-img_path = os.path.join(image_folder_path, '{0:04d}'.format(image_number)+'.tif') 
-mask_folder_path = image_folder_path + '_pred/masks'
-mask_path = os.path.join(mask_folder_path, '{0:04d}'.format(image_number)+'.png')
+    img_path = os.path.join(image_folder_path, '{0:04d}'.format(image_number)+'.tif') 
+    mask_folder_path = image_folder_path + '_pred/masks'
+    mask_path = os.path.join(mask_folder_path, '{0:04d}'.format(image_number)+'.png')
 
-st.title('rat DRG image segmentation')
+    st.title('rat DRG image segmentation')
 
-st.header(image_type)
+    st.header(image_type)
 
-img = plt.imread(img_path)[::2, ::2]
-mask = plt.imread(mask_path)[::2, ::2]
+    img = plt.imread(img_path)[::2, ::2]
+    mask = plt.imread(mask_path)[::2, ::2]
 
-left, right = st.sidebar.columns(2)
-show_mask = left.checkbox("Mask")
-show_separate_mask = right.checkbox("separate mask")
+    left, right = st.sidebar.columns(2)
+    show_mask = left.checkbox("Mask")
+    show_separate_mask = right.checkbox("separate mask")
 
 
-gain = st.sidebar.slider('Gain', 0.0, 3.0, 1.0)
-bias = st.sidebar.slider('Bias (brightness)', 0.0, 0.5, 0.1)
-img = (img  - np.min(img ))/np.ptp(img )
-img = np.clip(img * gain + bias, 0, 1)
+    gain = st.sidebar.slider('Gain', 0.0, 3.0, 1.0)
+    bias = st.sidebar.slider('Bias (brightness)', 0.0, 0.5, 0.1)
+    img = (img  - np.min(img ))/np.ptp(img )
+    img = np.clip(img * gain + bias, 0, 1)
 
-mask_template = np.zeros([img.shape[0], img.shape[1], 3])
-mask_template[..., 2] = img
-mask_template[..., 1] = mask/2
-mask_template[..., 0] = 0.5
-mask_final = hsv2rgb(mask_template)
+    mask_template = np.zeros([img.shape[0], img.shape[1], 3])
+    mask_template[..., 2] = img
+    mask_template[..., 1] = mask/2
+    mask_template[..., 0] = 0.5
+    mask_final = hsv2rgb(mask_template)
 
-if show_mask:
-    st.image(image=mask_final)
-elif show_separate_mask:
-    st.image(image=mask)
+    if show_mask:
+        st.image(image=mask_final)
+    elif show_separate_mask:
+        st.image(image=mask)
+    else:
+        st.image(image=img)
+
+
+
+
+    #st.image(image=mask)
+    #st.pyplot(fig)
+
+    '''
+    Image segmentation was performed with Deepflash2, an open source Deep-Learning tool for biological image segmentation
+    "https://matjesg.github.io/deepflash2/"
+    '''
+
+    '''
+    For visual purposes, original images were normalized by the min-max values of the respective image
+    '''
 else:
-    st.image(image=img)
-
-
-
-
-#st.image(image=mask)
-#st.pyplot(fig)
-
-'''
-Image segmentation was performed with Deepflash2, an open source Deep-Learning tool for biological image segmentation
-"https://matjesg.github.io/deepflash2/"
-'''
-
-'''
-For visual purposes, original images were normalized by the min-max values of the respective image
-'''
-
+    st.header('Images not provided in example dataset')
